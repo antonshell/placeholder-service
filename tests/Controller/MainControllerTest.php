95640301041fbb3b293e3baae980a46b68b003c8
antonshell <antonshell@yandex.ru>
2020-07-29 08:12:53 +0300

    Implemented index action test

---
+++ tests/Controller/MainControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class MainControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $content = json_decode($client->getResponse()->getContent(), true);
+        $this->assertEquals(['status' => 'ok', 'service' => 'placeholder service'], $content);
+    }
+}
