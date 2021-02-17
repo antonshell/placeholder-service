f0b46590e192009545b26674927a53905eb614f6
antonshell <antonshell@yandex.ru>
2021-02-17 14:18:34 +0300

    Added GA_ENVIRONMENT environment variable

--- tests/Controller/MainControllerTest.php
+++ tests/Controller/MainControllerTest.php
@@ -57,7 +57,7 @@ class MainControllerTest extends WebTestCase
 
         $expectedFilesDir = __DIR__ . '/../../resources/test_images';
 
-        if (isset($_ENV['DOCKER_ENVIRONMENT'])) {
+        if (isset($_ENV['DOCKER_ENVIRONMENT']) || isset($_ENV['GA_ENVIRONMENT'])) {
             $expectedFilesDir = __DIR__ . '/../../resources/test_images_docker';
         }
 
