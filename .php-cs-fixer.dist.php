3a74e0b336e7633855582e5df07bbf3e260f0947
Anton Shelyganov <antonshell@yandex.ru>
2021-05-30 22:11:28 +0300

    Updated cs-fixer config

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -6,25 +6,27 @@ $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__ . '/src')
     ->in(__DIR__ . '/tests');
 
-return PhpCsFixer\Config::create()
+$config = new PhpCsFixer\Config();
+$config
     ->setRiskyAllowed(true)
     ->setRules([
         '@Symfony' => true,
         '@PHP71Migration:risky' => true,
         'concat_space' => ['spacing' => 'one'],
-        'psr0' => false,
+        'psr_autoloading' => false,
         'array_syntax' => ['syntax' => 'short'],
-        'class_definition' => ['multiLineExtendsEachSingleLine' => true],
+        'class_definition' => ['multi_line_extends_each_single_line' => true],
         'no_useless_else' => true,
         'ordered_imports' => ['sort_algorithm' => 'alpha'],
         'phpdoc_add_missing_param_annotation' => ['only_untyped' => true],
-        'no_short_echo_tag' => true,
+        'echo_tag_syntax' => true,
         'list_syntax' => ['syntax' => 'short'],
         'linebreak_after_opening_tag' => true,
         'void_return' => false,
         'phpdoc_summary' => false,
         'multiline_whitespace_before_semicolons' => ['strategy' => 'new_line_for_chained_calls'],
-        'visibility_required' => ['property', 'method', 'const'],
     ])
     ->setCacheFile(__DIR__ . '/.php_cs.cache')
     ->setFinder($finder);
+
+return $config;
