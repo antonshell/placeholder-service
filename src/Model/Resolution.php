35056208f183efeb4e789bec46ee08196f4a6844
antonshell <antonshell@yandex.ru>
2021-12-03 14:03:38 +0300

    Refactored Models

--- src/Model/Resolution.php
+++ src/Model/Resolution.php
@@ -6,37 +6,19 @@ namespace App\Model;
 
 class Resolution
 {
-    /**
-     * @var int
-     */
-    private $width;
-
-    /**
-     * @var int
-     */
-    private $height;
+    public function __construct(
+        private int $width,
+        private int $height
+    ) {
+    }
 
     public function getWidth(): int
     {
         return $this->width;
     }
 
-    public function setWidth(int $width): Resolution
-    {
-        $this->width = $width;
-
-        return $this;
-    }
-
     public function getHeight(): int
     {
         return $this->height;
-    }
-
-    public function setHeight(int $height): Resolution
-    {
-        $this->height = $height;
-
-        return $this;
     }
 }
